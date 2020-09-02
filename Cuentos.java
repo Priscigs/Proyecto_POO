cc9cc8eb2ad70dac271c2005d5f785781505a6f1
jfaguilar01 <70551891+jfaguilar01@users.noreply.github.com>
2020-09-02 09:20:13 -0600

    Add files via upload

---
+++ Cuentos.java
@@ -0,0 +1,103 @@
+package Cuentos;
+
+/**
+ * cuentos
+ */
+public class Cuentos {
+    public Cuentos(){}
+    public String menuCuentosString(){
+        String menu = "\n Seleccione el cuento que desea leer. \n 1. Pinocho \n 2.La liebre y la tortuga.  \n 3.El patito feo  \n 4. Salir\n";
+        return menu;
+    }
+    public String cuento1(){
+        String cuento1;
+        cuento1="Pincho\n"+
+        "En una vieja carpintería, Geppetto, un señor amable y simpático, terminaba un día más\n"+
+        " de trabajo dando los últimos retoques de pintura a un muñeco de madera que había construido.\n"+"\n"
+        +"Al mirarlo, pensó: ¡qué bonito me ha quedado! Y como el muñeco había sido hecho de madera de pino, Geppetto decidió llamarlo Pinocho.\n"+
+        " Aquella noche, Geppeto se fue a dormir deseando que su muñeco fuese un niño de verdad.\n"+"\n"+
+        "Siempre había deseado tener un hijo. Y al encontrarse profundamente dormido, llegó un hada buena y viendo a Pinocho tan bonito,\n"+
+        "quiso premiar al buen carpintero, dando, con su varita mágica, vida al muñeco.\n"+"\n"+
+        "Al día siguiente, cuando se despertó, Geppetto no daba crédito a sus ojos: Pinocho se movía, caminaba, se reía y hablaba como un niño de verdad,\n" +
+        "para alegría del viejo carpintero.\n"+
+        "Feliz y muy satisfecho, Geppeto mandó a Pinocho a la escuela. Quería que fuese un niño muy listo y que aprendiera muchas cosas.\n"+
+        " Le acompañó su amigo Pepito Grillo, el consejero que le había dado el hada buena.\n\n"+
+        "Pero, en el camino del colegio, Pinocho se hizo amigo de dos niños muy malos, siguiendo sus travesuras,\n"+
+        "e ignorando los consejos del grillito. En lugar de ir a la escuela, Pinocho decidió seguir a sus nuevos amigos, buscando aventuras no muy buenas.\n"+
+        "Al ver esta situación, el hada buena le hechizó. Por no ir a la escuela, le colocó dos orejas de burro, y por portarse mal, \n"+
+        "le dijo que cada vez que dijera una mentira, le crecería la nariz, poniéndosele además colorada.\n\n"+
+        "Pinocho acabó reconociendo que no estaba siendo bueno, y arrepentido decidió buscar a Geppetto.\n"+
+        "Supo entonces que Geppeto, al salir en su busca por el mar, había sido tragado por una enorme ballena. \n"+
+        "Pinocho, con la ayuda del grillito, se fue a la mar para rescatar al pobre viejecito.\n\n"+
+        "Cuando Pinocho estuvo frente a la ballena le pidió que le devolviese a su papá, \n"+
+        "pero la ballena abrió su enorme boca y se lo tragó también a él.\n"+
+        "Dentro de la tripa de la ballena, Geppetto y Pinocho se reencontraron. \n"+
+        "Y se pusieran a pensar cómo salir de allí.\n\n"+
+        "Y gracias a Pepito Grillo encontraron una salida. Hicieron una fogata. El fuego hizo estornudar a la enorme ballena,\n"+
+        "y la balsa salió volando con sus tres tripulantes.\n"+
+        "Todos se salvaron. Pinocho volvió a casa y al colegio, y a partir de ese día siempre se comportó bien. Y en recompensa de su bondad,\n"+
+        "el hada buena lo convirtió en un niño de carne y hueso, y fueron muy felices por muchos y muchos años.\n"+
+        "FIN";
+        return cuento1;
+    }
+
+    public String cuento2(){
+        String cuento2;
+        cuento2="La liebre y la tortuga.\n"+
+        "En el mundo de los animales vivía una liebre muy orgullosa y vanidosa, que no cesaba de pregonar que ella era\n"+
+        "el animal más veloz del bosque, y que se pasaba el día burlándose de la lentitud de la tortuga.\n\n"+
+        "- ¡Eh, tortuga, no corras tanto! Decía la liebre riéndose de la tortuga.\n"+
+        "Un día, a la tortuga se le ocurrió hacerle una inusual apuesta a la liebre:\n"+
+        "- Liebre, ¿vamos hacer una carrera? Estoy segura de poder ganarte.\n"+
+        "- ¿A mí? Preguntó asombrada la liebre.\n"+
+        "- Sí, sí, a ti, dijo la tortuga. Pongamos nuestras apuestas y veamos quién gana la carrera.\n"+
+        "La liebre, muy engreída, aceptó la apuesta prontamente.\n"+
+        "Así que todos los animales se reunieron para presenciar la carrera. El búho ha sido el responsable \n"+
+        "de señalizar los puntos de partida y de llegada. Y así empezó la carrera:\n"+
+        "Astuta y muy confiada en sí misma, la liebre salió corriendo, y la tortuga se quedó atrás, tosiendo y envuelta en una nube de polvo.\n"+
+        "Cuando empezó a andar, la liebre ya se había perdido de vista. Sin importarle la ventaja que tenía la liebre sobre ella, la tortuga seguía su ritmo, sin parar.\n\n"+
+        "La liebre, mientras tanto, confiando en que la tortuga tardaría mucho en alcanzarla, se detuvo a la mitad del camino ante un frondoso y verde árbol,\n"+
+        "y se puso a descansar antes de terminar la carrera. Allí se quedó dormida, mientras la tortuga seguía caminando, paso tras paso, lentamente, pero sin detenerse.\n"+
+        "No se sabe cuánto tiempo la liebre se quedó dormida, pero cuando ella se despertó, vio con pavor que la tortuga se encontraba a tan solo tres pasos de la meta.\n"+
+        "En un sobresalto, salió corriendo con todas sus fuerzas, pero ya era muy tarde: ¡la tortuga había alcanzado la meta y ganado la carrera!\n"+
+        "Ese día la liebre aprendió, en medio de una gran humillación, que no hay que burlarse jamás de los demás. También aprendió que el exceso de confianza y de vanidad,\n"+
+        "es un obstáculo para alcanzar nuestros objetivos. Y que nadie, absolutamente nadie, es mejor que nadie.\n"+"FIN";
+        return cuento2;
+    }
+
+    public String cuento3(){
+        String cuento3;
+        cuento3="El patito feo\n\n"+
+        "En una hermosa mañana de verano, los huevos que habían empollado la mamá Pata empezaban a romperse, uno a uno.\n"+
+        "Los patitos fueron saliendo poquito a poco, llenando de felicidad a los papás y a sus amigos.\n"+
+        "Estaban tan contentos que casi no se dieron cuenta de que un huevo, el más grande de todos, aún permanecía intacto.\n\n"+
+        "Todos, incluso los patitos recién nacidos, concentraron su atención en el huevo, a ver cuando se rompería.\n"+
+        "Al cabo de algunos minutos, el huevo empezó a moverse, y luego se pudo ver el pico, luego el cuerpo, y las patas del sonriente pato.\n"+
+        "Era el más grande, y para sorpresa de todos, muy distinto de los demás. Y como era diferente, todos empezaron a llamarle el Patito Feo.\n\n"+
+        "La mamá Pata, avergonzada por haber tenido un patito tan feo, le apartó con el ala mientras daba atención a los otros patitos.\n"+
+        "El patito feo empezó a darse cuenta de que allí no le querían. Y a medida que crecía, se quedaba aún mas feo, y tenía que soportar las burlas de todos.\n"+
+        "Entonces, en la mañana siguiente, muy temprano, el patito decidió irse de la granja.\n\n"+
+        "Triste y solo, el patito siguió un camino por el bosque hasta llegar a otra granja. Allí, una vieja granjera le recogió, le dio de comer y beber,\n"+
+        "y el patito creyó que había encontrado a alguien que le quería. Pero, al cabo de algunos días, él se dio cuenta de que la vieja era mala y \n"+
+        "sólo quería engordarle para transformarlo en un segundo plato. \n"+
+        "El patito salió corriendo como pudo de allí.\n\n"+
+        "El invierno había llegado, y con él, el frío, el hambre y la persecución de los cazadores para el patito feo. Lo pasó muy mal. Pero sobrevivió hasta la llegada de la primavera.\n"+
+        "Los días pasaron a ser más calurosos y llenos de colores. Y el patito empezó a animarse otra vez. Un día, al pasar por un estanque, vio las aves más hermosas que jamás había visto.\n"+
+        "Eran elegantes, delicadas, y se movían como verdaderas bailarinas, por el agua. El patito, aún acomplejado por la figura y la torpeza que tenía,\n"+
+        "se acercó a una de ellas y le preguntó si podía bañarse también en el estanque.\n\n"+
+        "Y uno de los cisnes le contestó:\n"+
+        "- Pues, ¡claro que sí! Eres uno de los nuestros.\n"+
+        "Y le dijo el patito:\n"+
+        "- ¿Cómo que soy uno de los vuestros?\n"+
+        "Yo soy feo y torpe, todo lo contrario de vosotros.\n"+
+        "Y ellos le dijeron:\n"+
+        "- Entonces, mira tu reflejo en el agua del estanque y verás cómo no te engañamos.\n\n"+
+        "El patito se miró y lo que vio le dejó sin habla.\n"+
+        "¡Había crecido y se transformado en un precioso cisne! Y en este momento, él supo que jamás había sido feo.\n"+
+        "Él no era un pato sino un cisne. Y así, el nuevo cisne se unió a los demás y vivió feliz para siempre.\n"+
+        "FIN\n\n";
+
+        return cuento3;
+    }
+    
+}
